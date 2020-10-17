1842e2d6d8aa09db00070aa6295a15e4193ba33d
Josh8903 <thorngren87@gmail.com>
2020-10-17 18:31:59 -0500

    added console log nav cues

--- app.js
+++ app.js
@@ -4,121 +4,14 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
 const render = require("./lib/htmlRenderer");
 const { getUnpackedSettings } = require("http2");
-
 const employees = [];
 
-const internPrompt = [{
-        type: "input",
-        message: "Intern's name:",
-        name: "name"
-    },
-    {
-        type: "input",
-        message: "Intern's employee ID:",
-        name: "id"
-    },
-    {
-        type: "input",
-        message: "Intern's e-mail:",
-        name: "email"
-    },
-    {
-        type: "input",
-        message: "Intern's school:",
-        name: "school",
-    },
-    {
-        type: "list",
-        message: "What type of employee would you like to add next?",
-        name: "nextType",
-        choices: [
-            "Intern",
-            "Engineer",
-            "DONE"
-        ]
-    }
-];
-
-const engineerPrompt = [{
-        type: "input",
-        message: "Engineer's name:",
-        name: "name"
-    },
-    {
-        type: "input",
-        message: "Engineer's employee ID:",
-        name: "id"
-    },
-    {
-        type: "input",
-        message: "Engineer's e-mail:",
-        name: "email"
-    },
-    {
-        type: "input",
-        message: "Engineers's github:",
-        name: "github",
-    },
-    {
-        type: "list",
-        message: "What type of employee would you like to add next?",
-        name: "nextType",
-        choices: [
-            "Intern",
-            "Engineer",
-            "DONE"
-        ]
-    }
-];
-
-const nextIntern = function(response) {
-    employees.push(new Intern(response.name, response.id, response.email, response.school))
-    if (response.nextType === "Intern") {
-        inquirer
-            .prompt(internPrompt)
-            .then(nextIntern);
-    }
-    if (response.nextType === "Engineer") {
-        inquirer
-            .prompt(engineerPrompt)
-            .then(nextEngineer);
-    }
-    if (response.nextType === "DONE") {
-        fs.writeFile(outputPath, render(employees), function(err) {
-            if (err) throw err;
-            console.log("Generating HTML!");
-        });
-    }
-}
-
-const nextEngineer = function(response) {
-    employees.push(new Engineer(response.name, response.id, response.email, response.github))
-    if (response.nextType === "Intern") {
-        inquirer
-            .prompt(internPrompt)
-            .then(nextIntern);
-    }
-    if (response.nextType === "Engineer") {
-        inquirer
-            .prompt(engineerPrompt)
-            .then(nextEngineer);
-    }
-    if (response.nextType === "DONE") {
-        fs.writeFile(outputPath, render(employees), function(err) {
-            if (err) throw err;
-            console.log("Generating HTML!");
-        });
-    }
-}
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+// runs initialy, manager prompt
+console.log(`\n  ! Welcome to Employee Summary - a simple Node CLI app\n  ! that quickly creates a team interface in HTML!\n\n  ! Now adding new Manager`)
 inquirer
     .prompt([{
             type: "input",
@@ -153,46 +46,138 @@ inquirer
 
     ])
     .then(function(response) {
+        console.log(`  ! ${response.name} added`)
         employees.push(new Manager(response.name, response.id, response.email, response.officeNumber))
         if (response.nextType === "Intern") {
+            console.log(`  ! Now adding new ${response.nextType}`)
             inquirer
                 .prompt(internPrompt)
                 .then(nextIntern);
         }
         if (response.nextType === "Engineer") {
+            console.log(`  ! Now adding new ${response.nextType}`)
             inquirer
                 .prompt(engineerPrompt)
                 .then(nextEngineer);
         }
         if (response.nextType === "DONE") {
-            render(employees)
             fs.writeFile(outputPath, render(employees), function(err) {
                 if (err) throw err;
-                console.log("Generating HTML!");
+                console.log(`  ! Writing to ${outputPath}`);
             });
         }
     });
 
+// runs if add intern selected
+const nextIntern = function(response) {
+    console.log(`  ! ${response.name} added`)
+    employees.push(new Intern(response.name, response.id, response.email, response.school))
+    if (response.nextType === "Intern") {
+        console.log(`  ! Now adding new ${response.nextType}`)
+        inquirer
+            .prompt(internPrompt)
+            .then(nextIntern);
+    }
+    if (response.nextType === "Engineer") {
+        console.log(`  ! Now adding new ${response.nextType}`)
+        inquirer
+            .prompt(engineerPrompt)
+            .then(nextEngineer);
+    }
+    if (response.nextType === "DONE") {
+        fs.writeFile(outputPath, render(employees), function(err) {
+            if (err) throw err;
+            console.log(`  ! Writing to ${outputPath}`);
+        });
+    }
+}
 
+// runs if add engineer selected
+const nextEngineer = function(response) {
+    console.log(`  ! ${response.name} added`)
+    employees.push(new Engineer(response.name, response.id, response.email, response.github))
+    if (response.nextType === "Intern") {
+        console.log(`  ! Now adding new ${response.nextType}`)
+        inquirer
+            .prompt(internPrompt)
+            .then(nextIntern);
+    }
+    if (response.nextType === "Engineer") {
+        console.log(`  ! Now adding new ${response.nextType}`)
+        inquirer
+            .prompt(engineerPrompt)
+            .then(nextEngineer);
+    }
+    if (response.nextType === "DONE") {
+        fs.writeFile(outputPath, render(employees), function(err) {
+            if (err) throw err;
+            console.log(`  ! Writing to ${outputPath}`);
+        });
+    }
+}
 
+// intern prompt
+const internPrompt = [{
+        type: "input",
+        message: "Intern's name:",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "Intern's employee ID:",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "Intern's e-mail:",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "Intern's school:",
+        name: "school",
+    },
+    {
+        type: "list",
+        message: "What type of employee would you like to add next?",
+        name: "nextType",
+        choices: [
+            "Intern",
+            "Engineer",
+            "DONE"
+        ]
+    }
+];
 
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+// engineer prompt
+const engineerPrompt = [{
+        type: "input",
+        message: "Engineer's name:",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "Engineer's employee ID:",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "Engineer's e-mail:",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "Engineers's github:",
+        name: "github",
+    },
+    {
+        type: "list",
+        message: "What type of employee would you like to add next?",
+        name: "nextType",
+        choices: [
+            "Intern",
+            "Engineer",
+            "DONE"
+        ]
+    }
+];
