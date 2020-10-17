896890dbc798db28fab8ebe4f1460fde5ab67f84
Josh8903 <thorngren87@gmail.com>
2020-10-17 15:47:20 -0500

    fully functional, just needs refactor

--- app.js
+++ app.js
@@ -9,11 +9,173 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const { getUnpackedSettings } = require("http2");
 
+const employees = [];
 
+const internPrompt = [{
+        type: "input",
+        message: "Intern's name:",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "Intern's employee ID:",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "Intern's e-mail:",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "Intern's school:",
+        name: "school",
+    },
+    {
+        type: "list",
+        message: "What type of employee would you like to add next?",
+        name: "nextType",
+        choices: [
+            "Intern",
+            "Engineer",
+            "Done"
+        ]
+    }
+];
+
+const engineerPrompt = [{
+        type: "input",
+        message: "Engineer's name:",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "Engineer's employee ID:",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "Engineer's e-mail:",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "Engineers's github:",
+        name: "github",
+    },
+    {
+        type: "list",
+        message: "What type of employee would you like to add next?",
+        name: "nextType",
+        choices: [
+            "Intern",
+            "Engineer",
+            "DONE"
+        ]
+    }
+];
+
+const nextIntern = function(response) {
+    employees.push(new Intern(response.name, response.id, response.email, response.school))
+    if (response.nextType === "Intern") {
+        inquirer
+            .prompt(internPrompt)
+            .then(nextIntern);
+    }
+    if (response.nextType === "Engineer") {
+        inquirer
+            .prompt(engineerPrompt)
+            .then(nextEngineer);
+    }
+    if (response.nextType === "DONE") {
+        fs.writeFile(outputPath, render(employees), function(err) {
+            if (err) throw err;
+            console.log("Generating HTML!");
+        });
+    }
+}
+
+const nextEngineer = function(response) {
+    employees.push(new Engineer(response.name, response.id, response.email, response.github))
+    if (response.nextType === "Intern") {
+        inquirer
+            .prompt(internPrompt)
+            .then(nextIntern);
+    }
+    if (response.nextType === "Engineer") {
+        inquirer
+            .prompt(engineerPrompt)
+            .then(nextEngineer);
+    }
+    if (response.nextType === "DONE") {
+        fs.writeFile(outputPath, render(employees), function(err) {
+            if (err) throw err;
+            console.log("Generating HTML!");
+        });
+    }
+}
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+inquirer
+    .prompt([{
+            type: "input",
+            message: "Manager's name:",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "Manager's employee ID:",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "Manager's e-mail:",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "Manager's office number:",
+            name: "officeNumber",
+        },
+        {
+            type: "list",
+            message: "What type of employee would you like to add next?",
+            name: "nextType",
+            choices: [
+                "Intern",
+                "Engineer",
+                "DONE"
+            ]
+        }
+
+    ])
+    .then(function(response) {
+        employees.push(new Manager(response.name, response.id, response.email, response.officeNumber))
+        if (response.nextType === "Intern") {
+            inquirer
+                .prompt(internPrompt)
+                .then(nextIntern);
+        }
+        if (response.nextType === "Engineer") {
+            inquirer
+                .prompt(engineerPrompt)
+                .then(nextEngineer);
+        }
+        if (response.nextType === "DONE") {
+            render(employees)
+            fs.writeFile(outputPath, render(employees), function(err) {
+                if (err) throw err;
+                console.log("Generating HTML!");
+            });
+        }
+    });
+
+
+
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
